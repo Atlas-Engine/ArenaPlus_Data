1219fcd4d655671b5aeeb5252f748896a9b65df7
AerisDev <desroches.d@icloud.com>
2026-09-11 05:05:03 -0400

    Data 2026.09.11.0505

--- Cutoffs-tbc-us.lua
+++ Cutoffs-tbc-us.lua
@@ -12,14 +12,14 @@ local ns = ArenaPlusData
 -- Arena title cutoffs, written by tools\UpdateFromBlizzard.ps1 from Blizzard's
 -- own API. Do not edit by hand: rerun the script to refresh.
 --
--- Region tbc-us, season 3, cutoffs last changed 2026-09-10 11:13 AM, last checked 2026-09-11 03:43 AM.
+-- Region tbc-us, season 3, cutoffs last changed 2026-09-10 11:13 AM, last checked 2026-09-11 04:43 AM.
 ns.CUTOFFS_BY_REGION = ns.CUTOFFS_BY_REGION or {}
 
 ns.CUTOFFS_BY_REGION["tbc-us"] = {
 	region  = "tbc-us",
 	updated = "2026-09-10 11:13 AM",
-	checked = "2026-09-11 03:43 AM",
-	checkedEpoch = 1789112588,
+	checked = "2026-09-11 04:43 AM",
+	checkedEpoch = 1789116191,
 
 	[1] = { r1=2199, gladiator=2058, duelist=1863, rival=1676, challenger=1498 }, -- 2v2
 	[2] = { r1=2177, gladiator=2006, duelist=1844, rival=1690, challenger=1500 }, -- 3v3
@@ -31,7 +31,7 @@ ns.CUTOFFS_BY_REGION["tbc-us"] = {
 ns.CUTOFF_SLOTS_BY_REGION = ns.CUTOFF_SLOTS_BY_REGION or {}
 
 ns.CUTOFF_SLOTS_BY_REGION["tbc-us"] = {
-	[1] = { r1=22, gladiator=145, duelist=832, rival=3185 }, -- 2v2
-	[2] = { r1=28, gladiator=153, duelist=630, rival=2169 }, -- 3v3
-	[3] = { r1=53, gladiator=363, duelist=2253 }, -- 5v5
+	[1] = { r1=23, gladiator=157, duelist=848, rival=3204 }, -- 2v2
+	[2] = { r1=28, gladiator=153, duelist=637, rival=2186 }, -- 3v3
+	[3] = { r1=54, gladiator=373, duelist=2273 }, -- 5v5
 }
--- Cutoffs-us.lua
+++ Cutoffs-us.lua
@@ -12,14 +12,14 @@ local ns = ArenaPlusData
 -- Arena title cutoffs, written by tools\UpdateFromBlizzard.ps1 from Blizzard's
 -- own API. Do not edit by hand: rerun the script to refresh.
 --
--- Region us, season 14, cutoffs last changed 2026-09-10 10:15 AM, last checked 2026-09-11 04:00 AM.
+-- Region us, season 14, cutoffs last changed 2026-09-10 10:15 AM, last checked 2026-09-11 05:00 AM.
 ns.CUTOFFS_BY_REGION = ns.CUTOFFS_BY_REGION or {}
 
 ns.CUTOFFS_BY_REGION["us"] = {
 	region  = "us",
 	updated = "2026-09-10 10:15 AM",
-	checked = "2026-09-11 04:00 AM",
-	checkedEpoch = 1789113613,
+	checked = "2026-09-11 05:00 AM",
+	checkedEpoch = 1789117213,
 
 	[1] = { r1=2536, gladiator=2237, duelist=2075, rival=1812, challenger=1055 }, -- 2v2
 	[2] = { r1=2400, gladiator=1922, duelist=1849, rival=1619, challenger=864 }, -- 3v3
@@ -32,8 +32,8 @@ ns.CUTOFFS_BY_REGION["us"] = {
 ns.CUTOFF_SLOTS_BY_REGION = ns.CUTOFF_SLOTS_BY_REGION or {}
 
 ns.CUTOFF_SLOTS_BY_REGION["us"] = {
-	[1] = { r1=32, gladiator=207, duelist=520, rival=1666 }, -- 2v2
-	[2] = { r1=23, gladiator=188, duelist=215, rival=314, challenger=649 }, -- 3v3
-	[3] = { r1=16, gladiator=112, duelist=115, rival=135, challenger=199 }, -- 5v5
+	[1] = { r1=32, gladiator=207, duelist=521, rival=1667 }, -- 2v2
+	[2] = { r1=24, gladiator=188, duelist=215, rival=314, challenger=649 }, -- 3v3
+	[3] = { r1=17, gladiator=112, duelist=115, rival=135, challenger=200 }, -- 5v5
 	[4] = { r1=3, duelist=8, rival=27, challenger=120 }, -- rbg
 }
